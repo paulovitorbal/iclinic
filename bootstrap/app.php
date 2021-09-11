e2eb057fbff1843075ab4f01fe15ed9d58c5a0ac
Paulo Vitor Bettini de Paiva Lima <paulovitorbal@gmail.com>
2021-09-11 16:07:11 -0300

    refactor: removing unused class

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Flipbox\LumenGenerator\LumenGeneratorServiceProvider;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
@@ -51,7 +53,6 @@ $app->configure('app');
 |
 */
 
-$app->register(App\Providers\Psr15AdapterServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
